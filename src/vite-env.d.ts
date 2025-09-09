42a9508fe275c85a24ae8c9dcb3d815f074169af
kingtvland <42252946+menachem16@users.noreply.github.com>
2025-09-10 00:08:22 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_SHEETS_SPREADSHEET_ID: string
+  readonly VITE_GOOGLE_SHEETS_WEB_APP_URL: string
+  readonly VITE_GOOGLE_SHEETS_API_KEY: string
+  readonly VITE_GOOGLE_SHEETS_SHEET_NAME: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
